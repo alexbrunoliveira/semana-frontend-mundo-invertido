dab84b7e5470b669dd8060a32b036b85ce8d3c55
alexbrunoliveira <alexbrunoliveira@gmail.com>
2022-08-27 01:13:18 -0300

    Finalizando Projeto

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZGHo-cQWRqpIeA6hw3v6aPcsp3YyO-78",
+    authDomain: "projeto-teste-6c44d.firebaseapp.com",
+    projectId: "projeto-teste-6c44d",
+    storageBucket: "projeto-teste-6c44d.appspot.com",
+    messagingSenderId: "327113417964",
+    appId: "1:327113417964:web:54ddf5d81df804a9f8b1c9"
+  }
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+  export default app
